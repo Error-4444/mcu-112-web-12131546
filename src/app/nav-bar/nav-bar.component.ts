5a98972f0a88a6600a4ae9ce454fa4d3ce18a696
Error-4444 <aa0938396575@gmail.com>
2023-12-29 15:33:55 +0800

    feat(nav): 設定當前頁導覽按鈕樣式

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
