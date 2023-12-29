3646553997fc2f09acdc7ad934376e159cdce589
Error-4444 <aa0938396575@gmail.com>
2023-12-29 17:04:04 +0800

    feat(todo-form): 待辦事項編輯設定表單資料

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,15 +1,21 @@
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
-import { Component, EventEmitter, HostBinding, Output } from '@angular/core';
 import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Output,
+  Input,
+  OnChanges,
+} from '@angular/core';
+import {
+  FormArray,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
-  FormArray,
 } from '@angular/forms';
 import { ITodoForm } from '../interface/todo-form.interface';
 import { Todo } from '../model/todo';
-
 @Component({
   selector: 'app-todo-form',
   standalone: true,
@@ -17,36 +23,41 @@ import { Todo } from '../model/todo';
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css',
 })
-export class TodoFormComponent {
+export class TodoFormComponent implements OnChanges {
   @HostBinding('class')
   class = 'todo-form';
+
+  @Input()
+  initData?: Todo;
 
   @Output()
   readonly save = new EventEmitter<Todo>();
 
   @Output()
   readonly cancel = new EventEmitter<void>();
-
   readonly form = new FormGroup<ITodoForm>({
     content: new FormControl<string | null>(null, {
       validators: [Validators.required],
     }),
     tags: new FormArray<FormControl<string | null>>([]),
   });
-
   get formData(): Todo {
     return new Todo({
       content: this.content.value!,
       tags: this.tags.value.map((tag) => tag!),
     });
   }
-
   get content(): FormControl<string | null> {
     return this.form.get('content') as FormControl<string | null>;
   }
-
   get tags(): FormArray<FormControl<string | null>> {
     return this.form.get('tags') as FormArray<FormControl<string | null>>;
+  }
+
+  ngOnChanges(): void {
+    if (this.initData) {
+      this.form.patchValue(this.initData);
+    }
   }
 
   onAddTag(): void {
@@ -55,7 +66,6 @@ export class TodoFormComponent {
     });
     this.tags.push(control);
   }
-
   onSave(): void {
     this.save.emit(this.formData);
     this.form.reset();
